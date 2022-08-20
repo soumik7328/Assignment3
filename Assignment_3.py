a2788b78ce4a7cbb9cdcd8fd4ce1c4725e4f6d75
soumik7328 <111170984+soumik7328@users.noreply.github.com>
2022-08-20 11:11:05 +0530

    Add files via upload

---
+++ Assignment_3.py
@@ -0,0 +1,46 @@
+#write a python script to convert a number into str type
+a=10
+b=str(a)
+print(b)
+print(type(b))
+
+#write a python script to print unicode of the character "m"
+x='m'
+print(ord(x))
+
+#write a python script to print character representation of a given unicode 100
+x=100
+print(chr(x))
+#write a python script to print any number and it's binary equivalent
+x=100
+print("Number:-",x)
+print(bin(x))
+#write a python script to print any number and it's octal equivalent
+x=100
+print("Number:-",x)
+print(oct(x))
+
+#write a python script to print any number and it's hexadecimal equivalent
+x=100
+print("Number:-",x)
+print(hex(x))
+
+#write a python script to store binary number 1100101 in a variable and print it in decimal format
+x=0b1100101
+print(x)
+
+#write a python script to store hexadecimal number 2F in a variable and print it in octal format
+x=0x2F
+print(oct(x))
+
+#write a python script to store octal number 125 in a variable and print it in bionary format
+x=0o125
+print(bin(x))
+
+#write a python script to add two numbers 25(in octal) and 39 (in hexadecimal) and display the result in bionary format
+x=0o25
+y=0x39
+add=x+y
+print(bin(add))
+
+
